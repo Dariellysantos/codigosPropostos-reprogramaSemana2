9aced0432e658aaf657ccc0a3018663c6a568341
Darielly Santos <daryh_cristina@hotmail.com>
2021-08-26 12:13:52 -0300

    Atividade proposta no dia 21/08/21

---
+++ atividadeAlertArray.js
@@ -0,0 +1,24 @@
+//Exercicio 1
+alert ('Seja bem vindes ao meu site!')
+
+//exercicio 2
+const num1=87;
+const num2=96;
+
+let soma = (num1+num2)
+
+alert (soma)
+
+//exercicio 3
+const num3=5;
+const num4=8;
+
+let mult = (num3*num4)
+
+alert (mult)
+
+// exercicio 4
+// duvida: porque o alert da erro na leitura do console.log
+// porque a lista ta na vertical
+// const listaDeCompra = ['leite', 'carne' , 'biscoito' , 'manteiga' , 'ovo' , 'macarrão' , 'sal' , 'arroz']
+// console.log (listaDeCompra)
